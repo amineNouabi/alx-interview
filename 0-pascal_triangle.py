c9d389c994167b0b7bb5ce21fd7bf69be1556d72
amineNouabi <aminenouabi9@gmail.com>
2024-10-05 17:32:34 +0100

    Pascal triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+"""
+
+Pascal's Triangle
+
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns pascal's triangle of height n
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(n - 1):
+        last_row = triangle[-1]
+        last_row_len = len(last_row)
+        new_row = []
+        for j in range(last_row_len + 1):
+            a, b = 0, 0
+            if (j - 1 > -1):
+                a = last_row[j - 1]
+            if (j < last_row_len):
+                b = last_row[j]
+            new_row.append(a + b)
+        triangle.append(new_row)
+    return triangle
