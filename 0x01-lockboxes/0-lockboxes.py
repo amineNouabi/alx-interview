47f547a4460996e80169d85ca81700e560b6c8cd
Ahmed Amine Nouabi <42625141+amineNouabi@users.noreply.github.com>
2024-10-08 01:40:33 +0000

    lock boxes problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+
+Solution for lock boxes problem
+
+"""
+
+def unlock(boxes, openedBoxes, currentIndex):
+    """ Recursive unlocking of boxes """
+    if currentIndex < 0 or currentIndex >= len(boxes) or openedBoxes[currentIndex]:
+        return
+    
+    openedBoxes[currentIndex] = True
+    for boxIndex in boxes[currentIndex]:
+        unlock(boxes, openedBoxes, boxIndex)
+
+def canUnlockAll(boxes):
+    """ Returns True if can open all boxes """
+    openedBoxes = [False] * len(boxes)
+
+    unlock(boxes, openedBoxes, 0)
+    
+    for opened in openedBoxes:
+        if not opened:
+            return False
+    return True
