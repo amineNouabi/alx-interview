b8934182fc0a6da221c70706409a3dde80cdebd4
amineNouabi <aminenouabi9@gmail.com>
2024-10-08 08:16:24 +0100

    Fixed pep8

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,21 +6,23 @@ Solution for lock boxes problem
 
 """
 
+
 def unlock(boxes, openedBoxes, currentIndex):
     """ Recursive unlocking of boxes """
     if currentIndex < 0 or currentIndex >= len(boxes) or openedBoxes[currentIndex]:
         return
-    
+
     openedBoxes[currentIndex] = True
     for boxIndex in boxes[currentIndex]:
         unlock(boxes, openedBoxes, boxIndex)
+
 
 def canUnlockAll(boxes):
     """ Returns True if can open all boxes """
     openedBoxes = [False] * len(boxes)
 
     unlock(boxes, openedBoxes, 0)
-    
+
     for opened in openedBoxes:
         if not opened:
             return False
