d8bbd66f910b43126ca2b04c356e9566d31e74dd
amineNouabi <aminenouabi9@gmail.com>
2024-12-05 18:02:17 +0100

    redis dump done

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,12 +12,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if i > 0 and not grid[i - 1][j]:
+                if i > 0 and grid[i - 1][j] == 0:
                     perimeter += 1
-                if i < len(grid) - 1 and not grid[i + 1][j]:
+                if i < len(grid) - 1 and grid[i + 1][j] == 0:
                     perimeter += 1
-                if j > 0 and not grid[i][j - 1]:
+                if j > 0 and grid[i][j - 1] == 0:
                     perimeter += 1
-                if j < len(grid[i]) - 1 and not grid[i][j + 1]:
+                if j < len(grid[i]) - 1 and grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
